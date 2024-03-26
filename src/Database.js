4bd448dc809996d6f3f915a0bce0663506026b64
Penguini128 <tommy.tpg03@gmail.com>
2024-03-26 12:17:32 -0400

    basic frontend/backend interaction

--- src/Database.js
+++ src/Database.js
@@ -1,6 +1,10 @@
 const fs = require('fs').promises;
 const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
 
+
+// Loading recipes.json into MongoDB
 let recipeSchema = new mongoose.Schema({
     name: {type: String, default: ""},
     description: {type: String, default: ""},
@@ -10,33 +14,64 @@ let recipeSchema = new mongoose.Schema({
     prepTIme: {type: String, default: ""},
     ingredients: {type: [String], default: ""}
 })
-
 let Recipe = mongoose.model('Recipe', recipeSchema);
 
-mongoose.connect('mongodb://localhost:27017/RecipesDB')
-.then(() => {
-    console.log('Connected to MongoDB');
-    mongoose.connection.db.dropDatabase();
-}).then(() => {
-    let db = mongoose.connection;
-    db.on('open', function (err) {
-        if (err) throw err;
+
+
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('It\'s working!'))
+
+const port = 8080
+
+app.get('/test', (req, res) => {
+    res.json({ message: 'Data received GET request from the server'});
+});
+
+let diet = 'none';
+
+app.post('/populateDB', (req, res) => {
+    console.log("OH!");
+
+    mongoose.connect('mongodb://localhost:27017/RecipesDB')
+    .then(() => {
+        console.log('Connected to MongoDB');
+        mongoose.connection.db.dropDatabase();
+    }).then(() => {
+        let db = mongoose.connection;
+        db.on('open', function (err) {
+            if (err) throw err;
+        })
+
+    }).then(() => (
+        fs.readFile('recipes.json')
+    )).then((response) => (
+        JSON.parse(response)
+    )).then((json) => {
+        const promises = json.map(recipe => {
+            return Recipe.create(recipe)
+            .then((result) => {console.log("Added recipe:", result.name)});
+        });
+        return Promise.all(promises);
+    }).then((data) => {
+        console.log('Done populating database!');
+        res.send({success: true, amount: data.length});
     })
-    
-}).then(() => (
-    fs.readFile('recipes.json')
-)).then((response) => (
-    JSON.parse(response)
-)).then((json) => {
-    const promises = json.map(recipe => {
-        return Recipe.create(recipe)
-        .then((result) => {console.log("Added recipe:", result.name)});
-    });
-    return Promise.all(promises);
-}).then(() => {
-    console.log('Done!');
-    process.exit();
 })
 
+app.get('/form', (req, res) => {
+    res.send(diet);
+})
+
+app.listen(port, () => {
+    console.log("Server is running on port", port)
+})
 
 
--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,51 @@
 import { createUseStyles } from 'react-jss';
+import { useState } from 'react';
 import style from './Style';
 
-function App() {
-  const classes = createUseStyles(style)();
+
+
+function Header() {
+    const classes = createUseStyles(style)();
+    const [dataBase, setDataBase] = useState({
+        status: false,
+        size: 0
+    });
+    function populateDB() {
+      console.log("Button clicked!")
+      try {
+          fetch('http://localhost:8080/populateDB', {
+              method: 'POST'
+          }).then((response) => (
+            response.json()
+          )).then((data) => {
+            if (data.success) {
+                setDataBase({
+                    status: true,
+                    size: data.amount
+                });
+                console.log('yep');
+                console.log(data.amount);
+              } else {
+                console.log('oops');
+                console.log(data);
+              }
+          })
+      } catch (error) {
+          console.log("Error:", error);
+      }
+    }
+
     return (
         <div className={classes.mainHeader}>
-            <a className={classes.headerItem}>
-                Populate DB
-            </a>
+            {
+                dataBase.status ? <p>{dataBase.size}</p> :
+                <a className={classes.headerItem} onClick={populateDB}>
+                    Populate DB
+                </a>
+            }
+            
         </div>
     );
 }
 
-export default App;
+export default Header;
